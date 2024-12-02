880e1be4e7504ff1e3376ebf95d55ca67e54958c
uokou <120153425+uokou@users.noreply.github.com>
2024-12-02 15:29:53 +0800

    Create button_ex.ino

---
+++ Arduino/exam/button_ex.ino
@@ -0,0 +1,33 @@
+//按下按鈕顯示紅燈，放開則閃爍綠燈10秒
+const int button = 2;
+const int Led_g = 10;
+const int Led_r = 11; 
+int buttonState, key = 0;
+
+void setup(){
+  pinMode(button,INPUT);
+  pinMode(Led_g,OUTPUT);
+  pinMode(LED_r,OUTPUT);
+}
+
+void loop(){
+  buttonState = digitalRead(button);
+
+  if(buttonState == HIGH){
+    digitalWrite(LED_r,HIGH);
+    key = 1;
+  }
+  else{
+    digitalWrite(LED_r,LOW);
+    if(key == 1){
+      for(int i = 0; i<10, i ++){
+        digitalWrite(LED_g,HIGH);
+        delay(1000);
+        digitalWrite(Led_g,LOW);
+        delay(1000);
+      }
+    }
+    key ++;
+  }
+  delay(10);
+}
